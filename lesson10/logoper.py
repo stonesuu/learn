0711d20623739311b4e2194d08c4aa1ac615d518
stonesuu <365549445@qq.com>
2016-05-23 17:12:19 +0800

    lesson 10

---
+++ lesson10/logoper.py
@@ -0,0 +1,37 @@
+import MySQLdb as mysql
+con = mysql.connect(db='gaolei',host='localhost',user='root',passwd='123456')
+cursor = con.cursor()
+def sqloper(sql,ret=None):
+    sql = sql
+    try:
+        cursor.execute(sql)
+    except Exception as e:
+        return 'error:%s' % e
+    else:
+        return ret
+
+if __name__ == '__main__':    
+    sta = {}
+    sta_list = []
+    resule_list = []
+    with open('log.log') as f:
+        for line in f:
+            tmp = line.split(' ')
+            key = (tmp[0],tmp[8])
+            sta[key] = sta.get(key,0)+1
+    for key,count in sta.items():
+        sta_list.append((key,count))
+    for line in sorted(sta_list,key=(lambda x:x[1]),reverse=True)[:100]:
+        sql = 'insert into url_sta (ip,status,count) values ("%s",%d,%d)' % (line[0][0],int(line[0][1]),line[1])
+        resule_list.append(sqloper(sql,'ok'))
+    sql = 'commit'
+    sqloper(sql)
+    print resule_list
+    sql = 'select * from url_sta'
+    sqloper(sql)
+    print cursor.fetchall()
+    
+                      
+    
+
+    
---
+++ lesson10/zuoye10.py
@@ -0,0 +1,136 @@
+#-*- coding: utf-8 -*-
+import logoper as log
+from flask import Flask,render_template,request,redirect,session,json
+app = Flask(__name__)
+app.secret_key = 'sadfagraegrgaregareghhqare'
+page = 0
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    global page
+    if 'user' in session:
+        return redirect('/url')
+    else:
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            namelist = []
+            name = request.form.get('user')
+            pwd = request.form.get('passwd')
+            sql = 'select username from user'
+            log.sqloper(sql)
+            for line in log.cursor.fetchall():
+                namelist.append(line[0])
+            if name in namelist:
+                sql = 'select password from user where username="%s"' % name
+                log.sqloper(sql)
+                if pwd == log.cursor.fetchone()[0]:
+                    session['user'] = name
+                    page = 0
+                    return redirect('/url')
+                else:
+                    return render_template('login.html',result='password error')
+            else:
+                return render_template('login.html',result='username not exist')
+
+
+
+            
+
+
+@app.route('/url')
+def url():
+    if 'user' in session:
+        return render_template('url_sta.html')
+    else:
+        return redirect('/login')
+
+@app.route('/user')
+def user():
+    if 'user' in session:
+        return render_template('user_manage.html')
+    else:
+        return redirect('/login')
+            
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/login')
+
+
+@app.route('/getsta')
+def getsta():
+    sql = 'select * from url_sta limit %d,20' % (page*20,)
+    log.sqloper(sql)
+    res = json.dumps(log.cursor.fetchall())
+    return res
+
+@app.route('/getpage')
+def getpage():
+    global page
+    oper = request.args.get('oper')
+    if oper == 'plus' and page < 4:
+        page += 1
+        return 'ok'
+    elif oper == 'delete' and page > 0:
+        page -= 1
+        return 'ok'
+    elif oper == 'turnto':
+        page = int(request.args.get('page'))
+        return 'ok'
+    else:
+        return 'error'
+
+@app.route('/getuser')
+def getuser():
+    sql = 'select * from user'
+    log.sqloper(sql)
+    res = json.dumps(log.cursor.fetchall())
+    return res
+
+@app.route('/adduser')
+def adduser():
+    user = request.args.get('user')
+    passwd = request.args.get('passwd')
+    sql = 'insert into user(username,password) values("%s","%s")' % (user,passwd)
+    try:
+        log.sqloper(sql)
+        log.sqloper('commit')
+    except:
+        return 'error'
+    else:
+        return 'ok'
+
+@app.route('/deluser')
+def deluser():
+    num = request.args.get('id')
+    sql = 'delete from user where id=%s' % num
+    try:
+        log.sqloper(sql)
+    except:
+        return 'error'
+    else:
+        return 'ok'
+
+@app.route('/update')
+def update():
+    num = request.args.get('id')
+    passwd = request.args.get('passwd')
+    sql = 'update user set password="%s" where id=%s' % (passwd,num)
+    try:
+        log.sqloper(sql)
+    except:
+        return 'error'
+    else:
+        return 'ok'
+    
+
+
+if __name__ == '__main__':
+
+    app.run(host = '0.0.0.0',port = 9023,debug = True)
+
+
+
